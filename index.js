89eacdb18d0fad3544fcdd179613011f2989431b
Victor Eyo <victor.eyo@gmail.com>
2020-05-20 10:11:52 -1200

    herokuv1

--- index.js
+++ index.js
@@ -26,6 +26,12 @@ app.get('/', (req, res) =>{
 
 app.use('/', authRoute)
 
-app.listen(3000, ()=>{
+let port = process.env.PORT;
+
+if(port == null || port == ""){
+    port = 3000
+}
+
+app.listen(port, ()=>{
     console.log('server is running successfully!')
 })
